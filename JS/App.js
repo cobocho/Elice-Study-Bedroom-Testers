1f4781c03b6c6756ae9da56cb6a943203a3f2056
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-03-22 01:07:55 +0900

    feat: 랜딩페이지 종료 구현

--- JS/App.js
+++ JS/App.js
@@ -1,9 +1,23 @@
 import LandingController from "./LandingController.js";
+import $ from "./Utils.js";
 
 class App {
   constructor() {
-    console.log("app");
+    this.state = {
+      seq: 0,
+      type: null,
+    };
     this.landingController = new LandingController();
+    this.init();
+  }
+  init() {
+    const $startBtn = $(".start-btn");
+    $startBtn.addEventListener("click", () => {
+      this.finishLandingPage();
+    });
+  }
+  finishLandingPage() {
+    this.landingController.removeLandingPage();
   }
 }
 
--- JS/LandingController.js
+++ JS/LandingController.js
@@ -3,22 +3,13 @@ import $ from "./Utils.js";
 
 export default class LandingController {
   constructor() {
-    console.log("LandingController");
     this.addBubbles(70);
-    this.changeToTestPage();
   }
   addBubbles(amount) {
     const $inner = $(".inner");
     for (let i = 0; i < amount; i++) {
       new Bubble($inner, "span");
     }
-  }
-  changeToTestPage() {
-    const $startBtn = $(".start-btn");
-    $startBtn.addEventListener("click", () => {
-      console.log("start");
-      this.removeLandingPage();
-    });
   }
   removeLandingPage() {
     const $beer = $("section.beer-wrapper");
