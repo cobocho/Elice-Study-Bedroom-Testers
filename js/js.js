29925d990dd336af8b9cced02a4fff220af59e0d
puranium <puranium235@gmail.com>
2023-03-27 16:13:33 +0900

    progress bar, test logic

--- js/js.js
+++ js/js.js
@@ -2,10 +2,38 @@ const mainButton = document.getElementById("main-btn");
 const main = document.getElementById("main");
 const qna = document.getElementById("qna");
 const result = document.getElementById("result");
-function mainToTest(e){
+const menu1image = document.getElementById("menu1image");
+const menu1name = document.getElementById("menu1name");
+const menu2image = document.getElementById("menu2image");
+const menu2name = document.getElementById("menu2name");
+const menu1 = document.getElementById("menu1");
+const menu2 = document.getElementById("menu2");
+const progressBar = document.getElementById("bar-progress");
+
+function mainToTest(e) {
     e.preventDefault();
-    main.style.display = "none"
-    qna.style.display = "flex"
+    main.style.display = "none";
+    qna.style.display = "flex";
+}
+
+function testToResult(e) {
+    e.preventDefault();
+    qna.style.display = "none";
+    result.style.display = "flex";
+}
+
+let i = 0;
+let menuIndex = [...Array(menuList.length).keys()].sort(() => Math.random() - 0.5).slice(0, 32);
+
+function progress(idx) {
+    progressBar.style.width = `${idx * 3.3}%`;
+}
+
+function nextMenu(idx) {
+    menu1name.innerText = menuList[menuIndex[idx * 2]].name;
+    menu1image.src = menuList[menuIndex[idx * 2]].src;
+    menu2name.innerText = menuList[menuIndex[idx * 2 + 1]].name;
+    menu2image.src = menuList[menuIndex[idx * 2 + 1]].src;
 }
 
 mainButton.addEventListener("mouseover", (e) => {
@@ -18,8 +46,27 @@ mainButton.addEventListener("mouseout", (e) => {
     mainButton.style.backgroundColor = "#f7f1e5";
     mainButton.style.color = "#898121";
 })
-mainButton.addEventListener("click", (e) =>{
-    e.preventDefault();
-    main.style.display = "none";
-    qna.style.display = "flex";
+mainButton.addEventListener("click", (e) => {
+    mainToTest(e);
+    progress(i);
+    nextMenu(i);
+    i++;
+});
+menu1.addEventListener("click", (e) => {
+    menuIndex.push(menuIndex[i * 2]);
+    if (i === 30) testToResult(e)
+    else {
+        progress(i);
+        nextMenu(i);
+        i++;
+    }
+})
+menu2.addEventListener("click", (e) => {
+    menuIndex.push(menuIndex[i * 2 + 1]);
+    if (i === 30) testToResult(e)
+    else {
+        progress(i);
+        nextMenu(i);
+        i++;
+    }
 })
--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,182 @@
-menuList = [
+const menuList = [
     {
         name: "백반",
-        src: ".img/baek-ban.jpg"
+        src: "./img/baek-ban.jpg"
+    },
+    {
+        name: "밥버거",
+        src: "./img/bap-burger.jpg"
+    },
+    {
+        name: "비빔밥",
+        src: "./img/bibimbap.jpg"
+    },
+    {
+        name: "볶음밥",
+        src: "./img/bokkeumbap.jpg"
+    },
+    {
+        name: "보쌈",
+        src: "./img/bossam.jpg"
+    },
+    {
+        name: "불고기",
+        src: "./img/bulgogi.jpg"
+    },
+    {
+        name: "부리또",
+        src: "./img/burrito.jpg"
+    },
+    {
+        name: "커리",
+        src: "./img/curry.jpg"
+    },
+    {
+        name: "덮밥",
+        src: "./img/deopbap.jpg"
+    },
+    {
+        name: "간장게장",
+        src: "./img/ganjang-gejang.jpg"
+    },
+    {
+        name: "김밥",
+        src: "./img/gimbap.jpg"
+    },
+    {
+        name: "국밥",
+        src: "./img/gukbap.jpg"
+    },
+    {
+        name: "해장국",
+        src: "./img/haejangguk.jpg"
+    },
+    {
+        name: "해물찜",
+        src: "./img/haemul-jjim.jpg"
+    },
+    {
+        name: "햄버거",
+        src: "./img/hamburger.jpg"
+    },
+    {
+        name: "회덮밥",
+        src: "./img/hoe-deopbap.jpg"
+    },
+    {
+        name: "짜장면",
+        src: "./img/jajangmyeon.jpg"
+    },
+    {
+        name: "라멘",
+        src: "./img/japanese-ramen.jpg"
+    },
+    {
+        name: "제육볶음",
+        src: "./img/jeyuk.jpg"
+    },
+    {
+        name: "짬뽕",
+        src: "./img/jjamppong.jpg"
+    },
+    {
+        name: "찜닭",
+        src: "./img/jjim-dak.jpg"
+    },
+    {
+        name: "쭈꾸미볶음",
+        src: "./img/jjukumi.jpg"
+    },
+    {
+        name: "족발",
+        src: "./img/jokbal.jpg"
+    },
+    {
+        name: "칼국수",
+        src: "./img/kalguksu.jpg"
+    },
+    {
+        name: "김치찌개",
+        src: "./img/kimchi-jjigae.jpg"
+    },
+    {
+        name: "도시락",
+        src: "./img/lunch-box.jpg"
+    },
+    {
+        name: "마라탕",
+        src: "./img/malatang.jpg"
+    },
+    {
+        name: "만두",
+        src: "./img/mandu.jpg"
+    },
+    {
+        name: "모밀",
+        src: "./img/memil-soba.jpg"
+    },
+    {
+        name: "냉면",
+        src: "./img/naengmyeon.jpg"
+    },
+    {
+        name: "팟타이",
+        src: "./img/pad-thai.jpg"
+    },
+    {
+        name: "파스타",
+        src: "./img/pasta.jpg"
+    },
+    {
+        name: "피자",
+        src: "./img/pizza.jpg"
+    },
+    {
+        name: "돈까스",
+        src: "./img/pork-cutlet.jpg"
+    },
+    {
+        name: "라면",
+        src: "./img/ramen.jpg"
+    },
+    {
+        name: "샌드위치",
+        src: "./img/sandwich.jpg"
+    },
+    {
+        name: "샤브샤브",
+        src: "./img/shabu-shabu.jpg"
+    },
+    {
+        name: "쌀국수",
+        src: "./img/ssalguksu.jpg"
+    },
+    {
+        name: "쌈밥",
+        src: "./img/ssambap.jpg"
+    },
+    {
+        name: "순두부찌개",
+        src: "./img/sundubu-jjigae.jpg"
+    },
+    {
+        name: "초밥",
+        src: "./img/sushi.jpg"
+    },
+    {
+        name: "타코",
+        src: "./img/taco.jpg"
+    },
+    {
+        name: "떡볶이",
+        src: "./img/tteokbokki.jpg"
+    },
+    {
+        name: "우동",
+        src: "./img/udon.jpg"
+    },
+    {
+        name: "야채곱창",
+        src: "./img/yachae-gopchang.jpg"
     }
 ]
